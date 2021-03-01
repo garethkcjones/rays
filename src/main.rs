f09f1f91b5502e9572219d7f7e6a34e4a360835e
Gareth Jones <gareth.k.jones@gmail.com>
2021-03-01 18:05:42 +0000

    Listing 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    const IMAGE_WIDTH: i32 = 256;
+    const IMAGE_HEIGHT: i32 = 256;
+
+    println!("P3");
+    println!("{} {}", IMAGE_WIDTH, IMAGE_HEIGHT);
+    println!("255");
+
+    for j in (0..IMAGE_HEIGHT).rev() {
+        for i in 0..IMAGE_WIDTH {
+            let r = f64::from(i) / f64::from(IMAGE_WIDTH - 1);
+            let g = f64::from(j) / f64::from(IMAGE_HEIGHT - 1);
+            let b = 0.25;
+
+            let r = (255.999 * r) as i32;
+            let g = (255.999 * g) as i32;
+            let b = (255.999 * b) as i32;
+
+            println!("{} {} {}", r, g, b);
+        }
+    }
 }
