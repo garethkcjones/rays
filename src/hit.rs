5b1c304a98e996b8be25395072ea896049a4e117
Gareth Jones <gareth.k.jones@gmail.com>
2021-03-02 18:57:03 +0000

    Listing 20

--- src/hit.rs
+++ src/hit.rs
@@ -26,3 +26,19 @@ impl HitRecord {
         }
     }
 }
+
+impl Hittable for Vec<Box<dyn Hittable>> {
+    fn hit(&self, r: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord> {
+        let mut temp_rec = None;
+        let mut closest_so_far = t_max;
+
+        for object in self {
+            if let Some(rec) = object.hit(r, t_min, closest_so_far) {
+                closest_so_far = rec.t;
+                temp_rec.replace(rec);
+            }
+        }
+
+        temp_rec
+    }
+}
