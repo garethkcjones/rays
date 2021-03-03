de408e69df7f5b7cad301bff6d7232f6a4ac4904
Gareth Jones <gareth.k.jones@gmail.com>
2021-03-03 15:48:02 +0000

    Listing 26

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use rand::prelude::*;
+
 mod colour;
 mod hit;
 mod ray;
@@ -8,3 +10,17 @@ pub use hit::{HitRecord, Hittable, HittableList};
 pub use ray::Ray;
 pub use sphere::Sphere;
 pub use vec3::Vec3;
+
+/**
+ * Returns a random `f64` in [0.0, 1.0).
+ */
+pub fn random_f64() -> f64 {
+    rand::thread_rng().gen()
+}
+
+/**
+ * Returns a random `f64` in [min, max).
+ */
+pub fn random_f64_in(min: f64, max: f64) -> f64 {
+    min + (max - min) * random_f64()
+}
