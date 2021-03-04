c9d0bce10b9ed5a7b14e9aa959f017744b674549
Gareth Jones <gareth.k.jones@gmail.com>
2021-03-04 17:59:19 +0000

    Listing 60

--- src/dielectric.rs
+++ src/dielectric.rs
@@ -1,4 +1,4 @@
-use crate::{Colour, HitRecord, Material, Ray};
+use crate::{random_f64, Colour, HitRecord, Material, Ray};
 
 #[derive(Clone, Debug)]
 pub struct Dielectric {
@@ -32,9 +32,11 @@ impl Material for Dielectric {
         let unit_direction = ray.direction.unit();
         let cos_theta = (-unit_direction).dot(normal).clamp(-1.0, 1.0);
         let sin_theta = (1.0 - cos_theta * cos_theta).sqrt();
-        let cannot_refract = refraction_ratio * sin_theta > 1.0;
 
-        let direction = if cannot_refract {
+        let cannot_refract = refraction_ratio * sin_theta > 1.0;
+        let random_reflect = reflectance(cos_theta, refraction_ratio) > random_f64();
+
+        let direction = if cannot_refract || random_reflect {
             unit_direction.reflect(normal)
         } else {
             unit_direction.refract(normal, refraction_ratio)
@@ -43,4 +45,12 @@ impl Material for Dielectric {
         let scattered = Ray { origin, direction };
         Some((scattered, ATTENUATION))
     }
+}
+
+#[must_use]
+fn reflectance(cosine: f64, ref_idx: f64) -> f64 {
+    // Use Schlick’s approximation for reflectance.
+    let r0 = (1.0 - ref_idx) / (1.0 + ref_idx);
+    let r0 = r0 * r0;
+    r0 + (1.0 - r0) * (1.0 - cosine).powi(5)
 }
--- src/main.rs
+++ src/main.rs
@@ -66,7 +66,7 @@ fn main() {
     let material_ground: Rc<dyn Material> = Rc::new(Lambertian2::new(Colour::new(0.8, 0.8, 0.0)));
     let material_centre: Rc<dyn Material> = Rc::new(Lambertian2::new(Colour::new(0.1, 0.2, 0.5)));
     let material_left: Rc<dyn Material> = Rc::new(Dielectric::new(1.5));
-    let material_right: Rc<dyn Material> = Rc::new(Metal::new(Colour::new(0.8, 0.6, 0.2), 1.0));
+    let material_right: Rc<dyn Material> = Rc::new(Metal::new(Colour::new(0.8, 0.6, 0.2), 0.0));
 
     world.push(Box::new(Sphere::new(
         Vec3::new(0.0, -100.5, -1.0),
