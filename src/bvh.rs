640ca897c09d25e2a627ad36b64dc144d8fe83d1
Gareth Jones <gareth.k.jones@gmail.com>
2021-03-11 21:22:45 +0000

    Listing 17

--- src/bvh.rs
+++ src/bvh.rs
@@ -1,9 +1,9 @@
-use crate::{Aabb, HitRecord, Hittable, HittableList, Ray};
-use std::{cmp::Ordering, sync::Arc};
+use crate::{random_i32_in, Aabb, HitRecord, Hittable, HittableList, Ray};
+use std::{cmp::Ordering, mem, sync::Arc};
 
 #[derive(Clone, Debug)]
 pub struct BvhNode {
-    bounding_box: Aabb,
+    bounding_box: Option<Aabb>,
     left: Arc<dyn Hittable>,
     right: Arc<dyn Hittable>,
 }
@@ -11,24 +11,59 @@ pub struct BvhNode {
 impl BvhNode {
     #[must_use]
     pub fn new(objects: &HittableList, time0: f64, time1: f64) -> Self {
-        Self::from_range(objects.clone(), 0, objects.len(), time0, time1)
+        Self::from_range(&mut objects.clone(), time0, time1)
     }
 
     #[must_use]
-    fn from_range(
-        _objects: HittableList,
-        _start: usize,
-        _end: usize,
-        _time0: f64,
-        _time1: f64,
-    ) -> Self {
-        todo!()
+    fn from_range(objects: &mut [Arc<dyn Hittable>], time0: f64, time1: f64) -> Self {
+        let axis = random_i32_in(0, 2);
+        let comparator = match axis {
+            0 => box_x_cmp,
+            1 => box_y_cmp,
+            2 => box_z_cmp,
+            _ => unreachable!(),
+        };
+
+        let (left, right): (Arc<dyn Hittable>, Arc<dyn Hittable>) = match objects.len() {
+            0 => unreachable!(),
+            1 => {
+                let left = Arc::clone(objects.first().unwrap());
+                let right = Arc::clone(objects.last().unwrap());
+                (left, right)
+            }
+            2 => {
+                let mut left = Arc::clone(objects.first().unwrap());
+                let mut right = Arc::clone(objects.last().unwrap());
+                if let Ordering::Greater = comparator(&left, &right) {
+                    mem::swap(&mut left, &mut right);
+                }
+                (left, right)
+            }
+            len => {
+                objects.sort_unstable_by(comparator);
+                let mid = len / 2;
+                let left = Arc::new(BvhNode::from_range(&mut objects[0..mid], time0, time1));
+                let right = Arc::new(BvhNode::from_range(&mut objects[mid..len], time0, time1));
+                (left, right)
+            }
+        };
+
+        let box_left = left.bounding_box(time0, time1);
+        let box_right = right.bounding_box(time0, time1);
+        let bounding_box = Aabb::surrounding_box(box_left, box_right);
+
+        Self {
+            bounding_box,
+            left,
+            right,
+        }
     }
 }
 
 impl Hittable for BvhNode {
     fn hit(&self, r: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord> {
-        if self.bounding_box.hit(r, t_min, t_max) {
+        if self.bounding_box.unwrap().hit(r, t_min, t_max) {
+            // FIXME: unwrap()...
             let left_hit = self.left.hit(r, t_min, t_max);
             let t_max = left_hit.as_ref().map_or(t_max, |rec| rec.t());
             let right_hit = self.right.hit(r, t_min, t_max);
@@ -39,7 +74,7 @@ impl Hittable for BvhNode {
     }
 
     fn bounding_box(&self, _time0: f64, _time1: f64) -> Option<Aabb> {
-        Some(self.bounding_box)
+        self.bounding_box
     }
 }
 
