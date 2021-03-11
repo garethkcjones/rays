9164f6128f696f99290248cbd5c83c42c68b29ed
Gareth Jones <gareth.k.jones@gmail.com>
2021-03-11 17:30:40 +0000

    Listing 16

--- src/bvh.rs
+++ src/bvh.rs
@@ -27,8 +27,15 @@ impl BvhNode {
 }
 
 impl Hittable for BvhNode {
-    fn hit(&self, _r: &Ray, _t_min: f64, _t_max: f64) -> Option<HitRecord> {
-        todo!()
+    fn hit(&self, r: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord> {
+        if self.bounding_box.hit(r, t_min, t_max) {
+            let left_hit = self.left.hit(r, t_min, t_max);
+            let t_max = left_hit.as_ref().map_or(t_max, |rec| rec.t());
+            let right_hit = self.right.hit(r, t_min, t_max);
+            right_hit.or(left_hit)
+        } else {
+            None
+        }
     }
 
     fn bounding_box(&self, _time0: f64, _time1: f64) -> Option<Aabb> {
