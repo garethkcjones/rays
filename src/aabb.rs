62771f870caf66b10629e8e1a3a88647c6f4cdd3
Gareth Jones <gareth.k.jones@gmail.com>
2021-03-11 16:34:37 +0000

    Listing 14

--- src/aabb.rs
+++ src/aabb.rs
@@ -14,6 +14,18 @@ impl Aabb {
     }
 
     #[must_use]
+    pub fn surrounding_box(box0: Option<Self>, box1: Option<Self>) -> Option<Self> {
+        match (box0, box1) {
+            (Some(box0), Some(box1)) => {
+                let min_box = box0.minimum.apply_with(box1.minimum, f64::min);
+                let max_box = box0.maximum.apply_with(box1.maximum, f64::max);
+                Some(Self::new(min_box, max_box))
+            }
+            _ => None,
+        }
+    }
+
+    #[must_use]
     pub fn hit(&self, r: Ray, t_min: f64, t_max: f64) -> bool {
         for a in 0..3 {
             let inv_d = 1.0 / r.direction[a];
