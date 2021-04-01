90116340ebddd736fc9628cf69e8cf199efa2fdf
Gareth Jones <gareth.k.jones@gmail.com>
2021-04-01 16:43:07 +0100

    Listing 49

--- src/material.rs
+++ src/material.rs
@@ -1,7 +1,7 @@
 mod dielectric;
 mod lambertian;
 mod metal;
-use crate::{Colour, HitRecord, Ray};
+use crate::{Colour, HitRecord, Ray, Vector};
 pub use dielectric::Dielectric;
 pub use lambertian::{Lambertian0, Lambertian1, Lambertian2};
 pub use metal::Metal;
@@ -10,4 +10,9 @@ use std::fmt::Debug;
 pub trait Material: Debug + Send + Sync {
     #[must_use]
     fn scatter(&self, ray: &Ray, rec: &HitRecord) -> Option<(Ray, Colour)>;
+
+    #[must_use]
+    fn emitted(&self, _u: f64, _v: f64, _p: Vector) -> Colour {
+        Colour::new(0.0, 0.0, 0.0)
+    }
 }
