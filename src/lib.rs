eac066c22d988c77ffe4ff9940f9716fb334c510
Gareth Jones <gareth.k.jones@gmail.com>
2021-04-01 21:36:23 +0100

    Move rendering from main.rs to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -18,5 +18,159 @@ pub use material::{
 };
 pub use perlin::Perlin;
 pub use ray::Ray;
+use std::{
+    io::{self, prelude::*},
+    sync::Arc,
+    thread,
+};
 pub use texture::{Chequered, Noise, OpaqueImage, SolidColour, Texture};
 pub use triplet::{Colour, Vector};
+
+#[must_use]
+fn ray_colour(r: &Ray, world: &dyn Hittable, depth: u32) -> Colour {
+    let black = Colour::new(0.0, 0.0, 0.0);
+    let white = Colour::new(1.0, 1.0, 1.0);
+    let blue = Colour::new(0.5, 0.7, 1.0);
+
+    // If we’ve exceeded the ray bounce limit, no more light is gathered.
+    if depth == 0 {
+        return black;
+    }
+
+    if let Some(rec) = world.hit(r, 0.001, f64::INFINITY) {
+        if let Some((scattered, attenuation)) = rec.material().scatter(&r, &rec) {
+            return attenuation * ray_colour(&scattered, world, depth - 1);
+        } else {
+            return black;
+        }
+    }
+
+    let unit_direction = r.direction.unit();
+    let t = 0.5 * (unit_direction.y() + 1.0);
+    (1.0 - t) * white + t * blue
+}
+
+#[must_use]
+fn render(
+    thread_num: u32,
+    world: &dyn Hittable,
+    cam: &Camera,
+    image_width: u32,
+    image_height: u32,
+    samples_per_pixel: u32,
+    max_depth: u32,
+) -> Box<[Colour]> {
+    let mut pixels = Vec::with_capacity(image_width as usize * image_height as usize);
+
+    for j in 0..image_height {
+        if thread_num == 0 {
+            let percent = (100.0 * f64::from(j) / f64::from(image_height)).round() as u32;
+            print!(
+                "\rMain thread scanlines remaining: {:5} ({:3}%)",
+                image_height - j,
+                percent
+            );
+            io::stdout().flush().expect("Error writing to stdout");
+        }
+
+        for i in 0..image_width {
+            let mut pixel_colour = Colour::new(0.0, 0.0, 0.0);
+            for _ in 0..samples_per_pixel {
+                let u = (f64::from(i) + random::f64()) / f64::from(image_width - 1);
+                let v = (f64::from(j) + random::f64()) / f64::from(image_height - 1);
+                let r = cam.get_ray(u, v);
+                pixel_colour += ray_colour(&r, world, max_depth);
+            }
+            pixels.push(pixel_colour);
+        }
+    }
+
+    if thread_num == 0 {
+        println!("\rMain thread scanlines remaining: {:5} ({:3}%)", 0, 100);
+    }
+
+    pixels.into_boxed_slice()
+}
+
+#[must_use]
+fn render_thread(
+    thread_num: u32,
+    world: Arc<dyn Hittable>,
+    cam: Arc<Camera>,
+    image_width: u32,
+    image_height: u32,
+    samples_per_pixel: u32,
+    max_depth: u32,
+) -> Box<[Colour]> {
+    render(
+        thread_num,
+        world.as_ref(),
+        cam.as_ref(),
+        image_width,
+        image_height,
+        samples_per_pixel,
+        max_depth,
+    )
+}
+
+#[must_use]
+pub fn run(
+    num_threads: u32,
+    samples_per_pixel: u32,
+    max_depth: u32,
+    image_width: u32,
+    image_height: u32,
+    world: Arc<dyn Hittable>,
+    cam: Arc<Camera>,
+) -> Box<[Colour]> {
+    let samples_per_thread = samples_per_pixel / num_threads;
+    let remaining_samples = samples_per_pixel % num_threads;
+
+    // Spawn threads.
+    let mut threads = Vec::with_capacity(num_threads as usize - 1);
+    for thread_num in 1..num_threads {
+        let samples_per_pixel = if thread_num <= remaining_samples {
+            samples_per_thread + 1
+        } else {
+            samples_per_thread
+        };
+        let world = Arc::clone(&world);
+        let cam = Arc::clone(&cam);
+        threads.push(thread::spawn(move || {
+            render_thread(
+                thread_num,
+                world,
+                cam,
+                image_width,
+                image_height,
+                samples_per_pixel,
+                max_depth,
+            )
+        }));
+    }
+
+    // This thread.
+    let mut pixels = render(
+        0,
+        world.as_ref(),
+        cam.as_ref(),
+        image_width,
+        image_height,
+        samples_per_thread,
+        max_depth,
+    );
+
+    // Join threads.
+    for (i, thread) in threads.into_iter().enumerate() {
+        print!("\rWaiting for thread {:2} of {}...", i + 2, num_threads);
+        io::stdout().flush().expect("Error writing to stdout");
+        let thread_pixels = thread.join().expect("Worker thread error");
+        assert_eq!(pixels.len(), thread_pixels.len());
+        for (pixel, thread_pixel) in pixels.iter_mut().zip(thread_pixels.iter()) {
+            *pixel += thread_pixel;
+        }
+    }
+    println!();
+
+    pixels
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,8 @@
 use rays::{
     output, random, Camera, Chequered, Colour, Dielectric, Hittable, Lambertian2, Metal,
-    MovingSphere, Noise, OpaqueImage, Ray, Sphere, Vector,
+    MovingSphere, Noise, OpaqueImage, Sphere, Vector,
 };
-use std::{
-    env,
-    io::{self, prelude::*},
-    sync::Arc,
-    thread,
-};
+use std::{env, sync::Arc};
 
 #[must_use]
 fn random_scene() -> Arc<dyn Hittable> {
@@ -109,93 +104,6 @@ fn earth() -> Arc<dyn Hittable> {
     let texture = OpaqueImage::new("earthmap.jpg");
     let material = Lambertian2::new(texture);
     Sphere::new(Vector::new(0.0, 0.0, 0.0), 2.0, material)
-}
-
-#[must_use]
-fn ray_colour(r: &Ray, world: &dyn Hittable, depth: u32) -> Colour {
-    let black = Colour::new(0.0, 0.0, 0.0);
-    let white = Colour::new(1.0, 1.0, 1.0);
-    let blue = Colour::new(0.5, 0.7, 1.0);
-
-    // If we’ve exceeded the ray bounce limit, no more light is gathered.
-    if depth == 0 {
-        return black;
-    }
-
-    if let Some(rec) = world.hit(r, 0.001, f64::INFINITY) {
-        if let Some((scattered, attenuation)) = rec.material().scatter(&r, &rec) {
-            return attenuation * ray_colour(&scattered, world, depth - 1);
-        } else {
-            return black;
-        }
-    }
-
-    let unit_direction = r.direction.unit();
-    let t = 0.5 * (unit_direction.y() + 1.0);
-    (1.0 - t) * white + t * blue
-}
-
-#[must_use]
-fn render(
-    thread_num: u32,
-    world: &dyn Hittable,
-    cam: &Camera,
-    image_width: u32,
-    image_height: u32,
-    samples_per_pixel: u32,
-    max_depth: u32,
-) -> Box<[Colour]> {
-    let mut pixels = Vec::with_capacity(image_width as usize * image_height as usize);
-
-    for j in 0..image_height {
-        if thread_num == 0 {
-            let percent = (100.0 * f64::from(j) / f64::from(image_height)).round() as u32;
-            print!(
-                "\rMain thread scanlines remaining: {:5} ({:3}%)",
-                image_height - j,
-                percent
-            );
-            io::stdout().flush().expect("Error writing to stdout");
-        }
-
-        for i in 0..image_width {
-            let mut pixel_colour = Colour::new(0.0, 0.0, 0.0);
-            for _ in 0..samples_per_pixel {
-                let u = (f64::from(i) + random::f64()) / f64::from(image_width - 1);
-                let v = (f64::from(j) + random::f64()) / f64::from(image_height - 1);
-                let r = cam.get_ray(u, v);
-                pixel_colour += ray_colour(&r, world, max_depth);
-            }
-            pixels.push(pixel_colour);
-        }
-    }
-
-    if thread_num == 0 {
-        println!("\rMain thread scanlines remaining: {:5} ({:3}%)", 0, 100);
-    }
-
-    pixels.into_boxed_slice()
-}
-
-#[must_use]
-fn render_thread(
-    thread_num: u32,
-    world: Arc<dyn Hittable>,
-    cam: Arc<Camera>,
-    image_width: u32,
-    image_height: u32,
-    samples_per_pixel: u32,
-    max_depth: u32,
-) -> Box<[Colour]> {
-    render(
-        thread_num,
-        world.as_ref(),
-        cam.as_ref(),
-        image_width,
-        image_height,
-        samples_per_pixel,
-        max_depth,
-    )
 }
 
 fn main() {
@@ -318,54 +226,15 @@ fn main() {
     let samples_per_pixel = 100;
     let max_depth = 50;
     let num_threads = 32;
-    let samples_per_thread = samples_per_pixel / num_threads;
-    let remaining_samples = samples_per_pixel % num_threads;
-
-    // Spawn threads.
-    let mut threads = Vec::with_capacity(num_threads as usize - 1);
-    for thread_num in 1..num_threads {
-        let samples_per_pixel = if thread_num <= remaining_samples {
-            samples_per_thread + 1
-        } else {
-            samples_per_thread
-        };
-        let world = Arc::clone(&world);
-        let cam = Arc::clone(&cam);
-        threads.push(thread::spawn(move || {
-            render_thread(
-                thread_num,
-                world,
-                cam,
-                image_width,
-                image_height,
-                samples_per_pixel,
-                max_depth,
-            )
-        }));
-    }
-
-    // This thread.
-    let mut pixels = render(
-        0,
-        world.as_ref(),
-        cam.as_ref(),
+    let pixels = rays::run(
+        num_threads,
+        samples_per_pixel,
+        max_depth,
         image_width,
         image_height,
-        samples_per_thread,
-        max_depth,
+        world,
+        cam,
     );
-
-    // Join threads.
-    for (i, thread) in threads.into_iter().enumerate() {
-        print!("\rWaiting for thread {:2} of {}...", i + 2, num_threads);
-        io::stdout().flush().expect("Error writing to stdout");
-        let thread_pixels = thread.join().expect("Worker thread error");
-        assert_eq!(pixels.len(), thread_pixels.len());
-        for (pixel, thread_pixel) in pixels.iter_mut().zip(thread_pixels.iter()) {
-            *pixel += thread_pixel;
-        }
-    }
-    println!();
 
     // Output.
     println!("Writing output...");
