327335518188e536b70f8ad0be5117e3196f466d
Gareth Jones <gareth.k.jones@gmail.com>
2021-04-03 00:25:16 +0100

    Listing 52

--- src/hit/rect.rs
+++ src/hit/rect.rs
@@ -30,8 +30,25 @@ impl XyRect {
 }
 
 impl Hittable for XyRect {
-    fn hit(&self, _r: &Ray, _t_min: f64, _t_max: f64) -> Option<HitRecord> {
-        todo!()
+    fn hit(&self, r: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord> {
+        let t = (self.k - r.origin.z()) / r.direction.z();
+        if t < t_min || t > t_max {
+            return None;
+        }
+
+        let x = r.origin.x() + t * r.direction.x();
+        let y = r.origin.y() + t * r.direction.y();
+        if x < self.x.0 || x > self.x.1 || y < self.y.0 || y > self.y.1 {
+            return None;
+        }
+
+        let p = r.at(t);
+        let normal = Vector::new(0.0, 0.0, 1.0);
+        let material = Arc::clone(&self.material);
+        let u = (x - self.x.0) / (self.x.1 - self.x.0);
+        let v = (y - self.y.0) / (self.y.1 - self.y.0);
+
+        Some(HitRecord::new(r, p, normal, material, t, u, v))
     }
 
     fn bounding_box(&self, _time0: f64, _time1: f64) -> Option<Aabb> {
