36a6ff12b47f1f08a8f62e55e5a0b1b6c450ec8e
Gareth Jones <gareth.k.jones@gmail.com>
2021-04-11 01:46:13 +0100

    Port RTIOW Listing 20 to Rust

--- src/hittable.rs
+++ src/hittable.rs
@@ -3,7 +3,7 @@ mod sphere;
 use crate::Ray;
 pub use hitrecord::HitRecord;
 pub use sphere::Sphere;
-use std::fmt::Debug;
+use std::{fmt::Debug, sync::Arc};
 
 /**
  * Trait for hittable objects.
@@ -11,4 +11,26 @@ use std::fmt::Debug;
 pub trait Hittable: Debug {
     #[must_use]
     fn hit(&self, r: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord>;
+}
+
+impl Hittable for [Arc<dyn Hittable>] {
+    fn hit(&self, r: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord> {
+        let mut rec = None;
+        let mut closest_so_far = t_max;
+
+        for object in self {
+            if let Some(temp_rec) = object.hit(r, t_min, closest_so_far) {
+                closest_so_far = temp_rec.t();
+                rec = Some(temp_rec);
+            }
+        }
+
+        rec
+    }
+}
+
+impl Hittable for Vec<Arc<dyn Hittable>> {
+    fn hit(&self, r: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord> {
+        self.as_slice().hit(r, t_min, t_max)
+    }
 }
