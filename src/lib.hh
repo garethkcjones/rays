ba165f2e2c0af6ae323d4d6197d4ba396083e54b
Gareth Jones <gareth.k.jones@gmail.com>
2021-04-11 15:43:50 +0100

    RTIOW Listing 23
    Add degrees_to_radians().

--- src/lib.hh
+++ src/lib.hh
@@ -1,7 +1,19 @@
 #pragma once
 
+#include <numbers>
 #include <ostream>
 
 namespace rays {
+	// Utility functions.
+	constexpr double degrees_to_radians(double degrees) noexcept;
+
+	// Main raytracer function.
 	void run(std::ostream &output, bool log);
+}
+
+inline constexpr auto rays::degrees_to_radians(double const degrees) noexcept
+	-> double
+{
+	constexpr auto deg2rad = std::numbers::pi / 180.0;
+	return degrees * deg2rad;
 }
