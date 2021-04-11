d908c6994d1fd1c7ec404c75b1f8bb22f86c53bd
Gareth Jones <gareth.k.jones@gmail.com>
2021-04-11 01:29:40 +0100

    RTIOW Listing 20
    Add a hittable list of hittable objects.

---
+++ src/hittable.cc
@@ -0,0 +1,25 @@
+#include "hittable.hh"
+
+#include <optional>
+
+#include "hittable_hitrecord.hh"
+#include "ray.hh"
+
+using namespace rays::hittable;
+
+auto HittableList::hit(Ray const &r, double const t_min, double const t_max)
+	const noexcept -> std::optional<HitRecord>
+{
+	std::optional<HitRecord> rec;
+	auto closest_so_far = t_max;
+
+	for (auto &&object : *this) {
+		auto const temp_rec = object->hit(r, t_min, closest_so_far);
+		if (temp_rec) {
+			closest_so_far = temp_rec->t();
+			rec = temp_rec;
+		}
+	}
+
+	return rec;
+}
--- src/hittable.hh
+++ src/hittable.hh
@@ -1,12 +1,15 @@
 #pragma once
 
+#include <memory>
 #include <optional>
+#include <vector>
 
 #include "hittable_hitrecord.hh"
 #include "ray.hh"
 
 namespace rays::hittable {
 	class Hittable;
+	class HittableList;
 }
 
 /*
@@ -19,4 +22,19 @@ class rays::hittable::Hittable {
 
 		virtual std::optional<HitRecord> hit(Ray const &r, double t_min,
 		                                     double t_max) const noexcept = 0;
+};
+
+/*
+ * Hittable object type for storing a list of hittable objects.
+ */
+class rays::hittable::HittableList final:
+	public Hittable,
+	public std::vector<std::shared_ptr<Hittable>>
+{
+public:
+
+	using vector::vector;
+
+	std::optional<HitRecord> hit(Ray const &r, double t_min, double t_max) const
+		noexcept override;
 };
