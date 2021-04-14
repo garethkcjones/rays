695d2bb8754dd01a9a6746beafb30e79e54f3866
Gareth Jones <gareth.k.jones@gmail.com>
2021-04-14 23:47:13 +0100

    RTIOW Listing 60
    Use Schlick’s approximation for reflectance.

--- src/material_dielectric.cc
+++ src/material_dielectric.cc
@@ -15,6 +15,19 @@
 using namespace rays::material;
 using rays::hittable::HitRecord;
 
+namespace {
+	/*
+	 * Use Schlick’s approximation for reflectance.
+	 */
+	constexpr auto reflectance(double const cosine, double const ref_idx)
+		noexcept -> double
+	{
+		auto r0 = (1.0 - ref_idx) / (1.0 + ref_idx);
+		r0 = r0 * r0;
+		return r0 + (1.0 - r0) * std::pow((1.0 - cosine), 5);
+	}
+}
+
 Dielectric::Dielectric(double const ir) noexcept:
 	ir_{ir}
 {
@@ -26,9 +39,11 @@ auto Dielectric::new_material(double const ir) -> std::shared_ptr<Material> {
 
 auto Dielectric::scatter(Ray const &r_in,
                          HitRecord const &rec,
-                         std::default_random_engine &/*rand_eng*/) const
+                         std::default_random_engine &rand_eng) const
 	-> std::optional<std::pair<Colour, Ray>>
 {
+	auto rand_dst = std::uniform_real_distribution{0.0, 1.0};
+
 	auto const attenuation = Colour{1.0, 1.0, 1.0};
 	auto const refraction_ratio = rec.front_face() ? (1.0 / ir_) : ir_;
 
@@ -38,7 +53,8 @@ auto Dielectric::scatter(Ray const &r_in,
 	auto const sin_theta = std::sqrt(1.0 - cos_theta * cos_theta);
 
 	auto const cannot_refract = refraction_ratio * sin_theta > 1.0;
-	auto const direction = cannot_refract
+	auto const direction = (cannot_refract
+	          || reflectance(cos_theta, refraction_ratio) > rand_dst(rand_eng))
 		? reflect(unit_direction, rec.normal())
 		: refract(unit_direction, rec.normal(), refraction_ratio);
 
