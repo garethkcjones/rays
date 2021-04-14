33eacde51c9a8e91de419fc340847ad02ef5d916
Gareth Jones <gareth.k.jones@gmail.com>
2021-04-14 23:55:05 +0100

    Port RTIOW Listing 60 to Rust

--- src/material/dielectric.rs
+++ src/material/dielectric.rs
@@ -1,5 +1,6 @@
 use super::Material;
 use crate::{Colour, HitRecord, Ray};
+use rand::prelude::*;
 use std::sync::Arc;
 
 /**
@@ -36,14 +37,24 @@ impl Material for Dielectric {
         let sin_theta = (1.0 - cos_theta * cos_theta).sqrt();
 
         let cannot_refract = refraction_ratio * sin_theta > 1.0;
-        let direction = if cannot_refract {
-            unit_direction.reflect(rec.normal())
-        } else {
-            unit_direction.refract(rec.normal(), refraction_ratio)
-        };
+        let direction =
+            if cannot_refract || reflectance(cos_theta, refraction_ratio) > thread_rng().gen() {
+                unit_direction.reflect(rec.normal())
+            } else {
+                unit_direction.refract(rec.normal(), refraction_ratio)
+            };
 
         let scattered = Ray::new(rec.p(), direction);
 
         Some((attenuation, scattered))
     }
+}
+
+/**
+ * Use Schlick’s approximation for reflectance.
+ */
+fn reflectance(cosine: f64, ref_idx: f64) -> f64 {
+    let r0 = (1.0 - ref_idx) / (1.0 + ref_idx);
+    let r0 = r0 * r0;
+    r0 + (1.0 - r0) * (1.0 - cosine).powi(5)
 }
