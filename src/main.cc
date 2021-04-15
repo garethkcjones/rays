06da589eb92bc67447081bf91dde2fd7614c0929
Gareth Jones <gareth.k.jones@gmail.com>
2021-04-15 16:20:56 +0100

    RTIOW Listing 70
    Render final scene.

--- src/main.cc
+++ src/main.cc
@@ -3,6 +3,8 @@
 #include <filesystem>
 #include <fstream>
 #include <iostream>
+#include <memory>
+#include <random>
 #include <stdexcept>
 #include <string>
 #include <utility>
@@ -19,11 +21,7 @@
 namespace fs = std::filesystem;
 
 namespace {
-	/*
-	 * Builds and renders a scene.
-	 */
-	void render(std::ostream &output) {
-		using rays::Camera;
+	auto random_scene() -> rays::hittable::HittableList {
 		using rays::Colour;
 		using rays::hittable::HittableList;
 		using rays::hittable::Sphere;
@@ -32,40 +30,99 @@ namespace {
 		using rays::material::Metal;
 		using rays::Vec3;
 
+		auto rand_dev = std::random_device{};
+		auto rand_eng = std::default_random_engine{rand_dev()};
+		auto rand_dst = std::uniform_real_distribution{0.0, 1.0};
+
+		HittableList world;
+
+		auto ground_material = Lambertian2::new_material(Colour{0.5, 0.5, 0.5});
+		world.push_back(Sphere::new_hittable(Vec3{0.0, -1000.0, 0.0}, 1000.0,
+			std::move(ground_material)));
+
+		for (auto a = -11; a < 11; ++a) {
+			for (auto b = -11; b < 11; ++b) {
+				auto const choose_mat = rand_dst(rand_eng);
+				auto const centre = Vec3 {
+					a + 0.9 * rand_dst(rand_eng),
+					0.2,
+					b + 0.9 * rand_dst(rand_eng)
+				};
+
+				if ((centre - Vec3{4.0, 0.2, 0.0}).length() > 0.9) {
+					if (choose_mat < 0.8) {
+						// Diffuse.
+						auto const albedo =
+							  Colour::new_random(rand_eng, 0.0, 1.0)
+							* Colour::new_random(rand_eng, 0.0, 1.0);
+						auto sphere_material =
+							Lambertian2::new_material(albedo);
+						world.push_back(Sphere::new_hittable(centre, 0.2,
+							std::move(sphere_material)));
+					} else if (choose_mat < 0.95) {
+						// Metal.
+						auto const albedo =
+							Colour::new_random(rand_eng, 0.5, 1.0);
+						auto const fuzz = 0.5 * rand_dst(rand_eng);
+						auto sphere_material =
+							Metal::new_material(albedo, fuzz);
+						world.push_back(Sphere::new_hittable(centre, 0.2,
+							std::move(sphere_material)));
+					} else {
+						// Glass.
+						auto sphere_material = Dielectric::new_material(1.5);
+						world.push_back(Sphere::new_hittable(centre, 0.2,
+							std::move(sphere_material)));
+					}
+				}
+			}
+		}
+
+		auto material1 = Dielectric::new_material(1.5);
+		world.push_back(Sphere::new_hittable(Vec3{0.0, 1.0, 0.0}, 1.0,
+			std::move(material1)));
+
+		auto material2 = Lambertian2::new_material(Colour{0.4, 0.2, 0.1});
+		world.push_back(Sphere::new_hittable(Vec3{-4.0, 1.0, 0.0}, 1.0,
+			std::move(material2)));
+
+		auto material3 = Metal::new_material(Colour{0.7, 0.6, 0.5}, 0.0);
+		world.push_back(Sphere::new_hittable(Vec3{4.0, 1.0, 0.0}, 1.0,
+			std::move(material3)));
+
+		return world;
+	}
+
+	/*
+	 * Builds and renders a scene.
+	 */
+	void render(std::ostream &output) {
+		using rays::Camera;
+		using rays::Vec3;
+
 		// Image.
 
-		constexpr auto image_aspect_ratio = 16.0 / 9.0;
-		constexpr auto image_width = 400;
+		constexpr auto image_aspect_ratio = 3.0 / 2.0;
+		constexpr auto image_width = 1200;
 		constexpr auto image_height =
 			static_cast<int>(image_width / image_aspect_ratio);
-		constexpr auto samples_per_pixel = 100;
+		constexpr auto samples_per_pixel = 500;
 		constexpr auto max_depth = 50;
 
 		// World.
 
-		auto material_ground = Lambertian2::new_material(Colour{0.8, 0.8, 0.0});
-		auto material_centre = Lambertian2::new_material(Colour{0.1, 0.2, 0.5});
-		auto material_left   = Dielectric::new_material(1.5);
-		auto material_right  = Metal::new_material(Colour{0.8, 0.6, 0.2}, 0.0);
-
-		HittableList world;
-
-		world.push_back(Sphere::new_hittable(Vec3{ 0.0, -100.5, -1.0}, 100.0, std::move(material_ground)));
-		world.push_back(Sphere::new_hittable(Vec3{ 0.0,    0.0, -1.0},   0.5, std::move(material_centre)));
-		world.push_back(Sphere::new_hittable(Vec3{-1.0,    0.0, -1.0},   0.5, material_left));
-		world.push_back(Sphere::new_hittable(Vec3{-1.0,    0.0, -1.0}, -0.45, std::move(material_left)));
-		world.push_back(Sphere::new_hittable(Vec3{ 1.0,    0.0, -1.0},   0.5, std::move(material_right)));
+		auto const world = random_scene();
 
 		// Camera
 
-		constexpr auto lookfrom = Vec3{3.0, 3.0,  2.0};
-		constexpr auto lookat   = Vec3{0.0, 0.0, -1.0};
-		constexpr auto vup      = Vec3{0.0, 1.0,  0.0};
+		constexpr auto lookfrom = Vec3{13.0, 2.0, 3.0};
+		constexpr auto lookat   = Vec3{ 0.0, 0.0, 0.0};
+		constexpr auto vup      = Vec3{ 0.0, 1.0, 0.0};
 		constexpr auto vfov = 20.0;
 		constexpr auto aspect_ratio =
 			static_cast<double>(image_width) / image_height;
-		constexpr auto aperture = 2.0;
-		auto const dist_to_focus = (lookfrom - lookat).length();
+		constexpr auto aperture = 0.1;
+		constexpr auto dist_to_focus = 10.0;
 
 		auto const cam = Camera {
 			lookfrom,
