219c33fb436f63ae6efcf52c08b39bdc1d1bf6e1
Gareth Jones <gareth.k.jones@gmail.com>
2021-04-17 22:41:56 +0100

    RTTNW Listing 2
    Camera with exposure time for motion blur.

--- src/camera.hh
+++ src/camera.hh
@@ -7,10 +7,11 @@
 
 namespace rays {
 	class Camera;
+	class LongExposureCamera;
 }
 
 /*
- * Type for representing a viewport.
+ * Type for representing an instantaneous viewport.
  */
 class rays::Camera final {
 	public:
@@ -23,6 +24,30 @@ class rays::Camera final {
 
 	private:
 
+		Vec3 origin_;
+		Vec3 horizontal_;
+		Vec3 vertical_;
+		Vec3 lower_left_corner_;
+		Vec3 u_, v_, w_;
+		double lens_radius_;
+};
+
+/*
+ * Type for representing a viewport over a period of time.
+ */
+class rays::LongExposureCamera final {
+	public:
+
+		explicit LongExposureCamera(Vec3 lookfrom, Vec3 lookat, Vec3 vup,
+			double vfov, double aspect_ratio, double aperture,
+			double focus_dist, double time0, double time1);
+
+		Ray get_ray(double u, double v, std::default_random_engine &rand_eng)
+			const;
+
+	private:
+
+		mutable std::uniform_real_distribution<double> time_range_;
 		Vec3 origin_;
 		Vec3 horizontal_;
 		Vec3 vertical_;
