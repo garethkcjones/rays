7343eeb32b55d125faba7791f73f9c79a68d8135
Gareth Jones <gareth.k.jones@gmail.com>
2021-04-20 00:29:09 +0100

    RTTNW Listings 27 & 28
    Render a scene with two chequered spheres.

--- src/main.cc
+++ src/main.cc
@@ -1,3 +1,4 @@
+#include <cstddef>
 #include <cstdlib>
 #include <exception>
 #include <filesystem>
@@ -109,38 +110,102 @@ namespace {
 		return world;
 	}
 
+	auto two_spheres() -> std::shared_ptr<rays::hittable::HittableList> {
+		using rays::Colour;
+		using rays::hittable::HittableList;
+		using rays::hittable::Sphere;
+		using rays::material::Lambertian2;
+		using rays::texture::Chequer;
+		using rays::Vec3;
+
+		auto const objects = std::make_shared<HittableList>();
+
+		auto chequer = Chequer::new_texture(
+			Vec3{10.0, 10.0, 10.0},
+			Colour{0.2, 0.3, 0.1},
+			Colour{0.9, 0.9, 0.9}
+		);
+
+		objects->push_back(Sphere::new_hittable(Vec3{0.0, -10.0, 0.0}, 10.0,
+			Lambertian2::new_material(chequer)));
+		objects->push_back(Sphere::new_hittable(Vec3{0.0, 10.0, 0.0}, 10.0,
+			Lambertian2::new_material(std::move(chequer))));
+
+		return objects;
+	}
+
 	/*
 	 * Builds and renders a scene.
 	 */
-	void render(std::ostream &output) {
+	void render(int const scene, std::ostream &output) {
 		using rays::Camera;
+		using rays::hittable::Hittable;
 		using rays::Vec3;
 
-		// Image.
+		// Scene parameters.
+		std::shared_ptr<Hittable> world;
+		Vec3 lookfrom, lookat, vup;
+		double vfov, aspect_ratio, aperture, dist_to_focus, time0, time1;
+		int image_width, image_height, samples_per_pixel, max_depth;
 
-		constexpr auto image_aspect_ratio = 16.0 / 9.0;
-		constexpr auto image_width = 400;
-		constexpr auto image_height =
-			static_cast<int>(image_width / image_aspect_ratio);
-		constexpr auto samples_per_pixel = 100;
-		constexpr auto max_depth = 50;
+		switch (scene) {
+			case 1: {
+				// Image.
+				auto const image_aspect_ratio = 16.0 / 9.0;
+				image_width = 400;
+				image_height =
+					static_cast<int>(image_width / image_aspect_ratio);
+				samples_per_pixel = 100;
+				max_depth = 50;
 
-		// World.
+				// World.
+				world = random_scene();
 
-		auto const world = random_scene();
+				// Camera.
+				lookfrom = Vec3{13.0, 2.0, 3.0};
+				lookat   = Vec3{ 0.0, 0.0, 0.0};
+				vup      = Vec3{ 0.0, 1.0, 0.0};
+				vfov = 20.0;
+				aspect_ratio = static_cast<double>(image_width) / image_height;
+				aperture = 0.1;
+				dist_to_focus = 10.0;
+				time0 = 0.0;
+				time1 = 1.0;
 
-		// Camera
+				break;
+			}
 
-		constexpr auto lookfrom = Vec3{13.0, 2.0, 3.0};
-		constexpr auto lookat   = Vec3{ 0.0, 0.0, 0.0};
-		constexpr auto vup      = Vec3{ 0.0, 1.0, 0.0};
-		constexpr auto vfov = 20.0;
-		constexpr auto aspect_ratio =
-			static_cast<double>(image_width) / image_height;
-		constexpr auto aperture = 0.1;
-		constexpr auto dist_to_focus = 10.0;
-		constexpr auto time0 = 0.0;
-		constexpr auto time1 = 1.0;
+			case 2: {
+				// Image.
+				auto const image_aspect_ratio = 16.0 / 9.0;
+				image_width = 400;
+				image_height =
+					static_cast<int>(image_width / image_aspect_ratio);
+				samples_per_pixel = 100;
+				max_depth = 50;
+
+				// World.
+				world = two_spheres();
+
+				// Camera.
+				lookfrom = Vec3{13.0, 2.0, 3.0};
+				lookat   = Vec3{ 0.0, 0.0, 0.0};
+				vup      = Vec3{ 0.0, 1.0, 0.0};
+				vfov = 20.0;
+				aspect_ratio = static_cast<double>(image_width) / image_height;
+				aperture = 0.0;
+				dist_to_focus = 10.0;
+				time0 = 0.0;
+				time1 = 1.0;
+
+				break;
+			}
+
+			default:
+				throw std::runtime_error {
+					"invalid scene number: " + std::to_string(scene)
+				};
+		}
 
 		auto const cam = std::make_shared<Camera>(
 			lookfrom,
@@ -171,6 +236,14 @@ namespace {
 		);
 	}
 
+	auto scene_number(std::string const arg) -> int {
+		std::size_t pos;
+		auto const scene = std::stoi(arg, &pos);
+		if (pos == arg.length())
+			return scene;
+		throw std::runtime_error{"invalid scene number “" + arg + "”"};
+	}
+
 	/*
 	 * Runs the program.
 	 */
@@ -178,14 +251,22 @@ namespace {
 		switch (argc) {
 			case 0:
 			case 1:
-				// No output file name specified on command-line.  Use stdout.
-				std::ios::sync_with_stdio(false);
-				render(std::cout);
-				break;
+				throw std::runtime_error{"no scene number specified"};
 
 			case 2: {
+				auto const scene = scene_number(argv[1]);
+
+				// No output file name specified on command-line.  Use stdout.
+				std::ios::sync_with_stdio(false);
+				render(scene, std::cout);
+				break;
+			}
+
+			case 3: {
+				auto const scene = scene_number(argv[1]);
+
 				// Get the output file name from the command-line.
-				auto const filename = fs::path{argv[1]};
+				auto const filename = fs::path{argv[2]};
 
 				auto output = std::ofstream{filename};
 				if (!output) {
@@ -194,7 +275,7 @@ namespace {
 					};
 				}
 
-				render(output);
+				render(scene, output);
 
 				if (!output.flush()) {
 					throw std::runtime_error {
