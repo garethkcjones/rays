e9c0d4d85fd1694e8187604ccc0d1f1c0616e472
Gareth Jones <gareth.k.jones@gmail.com>
2021-04-20 23:43:08 +0100

    Port RTTNW Listings 37–40 to Rust

--- src/perlin.rs
+++ src/perlin.rs
@@ -8,7 +8,7 @@ const POINT_COUNT: usize = 1 << 8;
  */
 #[derive(Clone, Debug)]
 pub struct Perlin {
-    ranfloat: [f64; POINT_COUNT],
+    ranvec: [Vec3; POINT_COUNT],
     perm_x: [usize; POINT_COUNT],
     perm_y: [usize; POINT_COUNT],
     perm_z: [usize; POINT_COUNT],
@@ -36,15 +36,11 @@ impl Perlin {
         let v = py - fpy;
         let w = pz - fpz;
 
-        let u = u * u * (3.0 - 2.0 * u);
-        let v = v * v * (3.0 - 2.0 * v);
-        let w = w * w * (3.0 - 2.0 * w);
-
         let i = fpx as i32;
         let j = fpy as i32;
         let k = fpz as i32;
 
-        let mut c = [[[0.0; 2]; 2]; 2];
+        let mut c = [[[Default::default(); 2]; 2]; 2];
 
         for di in 0..2 {
             let iterm = (i + di) as isize as usize & MASK;
@@ -59,26 +55,24 @@ impl Perlin {
                     let zterm = self.perm_z[kterm];
 
                     let ind = xterm ^ yterm ^ zterm;
-                    c[di as usize][dj as usize][dk as usize] = self.ranfloat[ind];
+                    c[di as usize][dj as usize][dk as usize] = self.ranvec[ind];
                 }
             }
         }
 
-        trilinear_interp(&c, u, v, w)
+        perlin_interp(&c, u, v, w)
     }
 }
 
 impl Default for Perlin {
     fn default() -> Self {
-        let mut ranfloat = [0.0; POINT_COUNT];
-        thread_rng().fill(&mut ranfloat);
-
+        let ranvec = rand_fill();
         let perm_x = perlin_generate_perm();
         let perm_y = perlin_generate_perm();
         let perm_z = perlin_generate_perm();
 
         Self {
-            ranfloat,
+            ranvec,
             perm_x,
             perm_y,
             perm_z,
@@ -102,20 +96,39 @@ fn perlin_generate_perm() -> [usize; POINT_COUNT] {
     p
 }
 
-fn trilinear_interp(c: &[[[f64; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+fn rand_fill() -> [Vec3; POINT_COUNT] {
+    let mut ranvec = [Default::default(); POINT_COUNT];
+    for i in &mut ranvec {
+        *i = Vec3::new_random(-1.0..1.0);
+    }
+    ranvec
+}
+
+fn perlin_interp(c: &[[[Vec3; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+    let uu = u * u * (3.0 - 2.0 * u);
+    let vv = v * v * (3.0 - 2.0 * v);
+    let ww = w * w * (3.0 - 2.0 * w);
+
     let mut accum = 0.0;
 
     #[allow(clippy::needless_range_loop)]
     for i in 0..2 {
         let fi = i as f64;
-        let iterm = fi * u + (1.0 - fi) * (1.0 - u);
+        let iterm = fi * uu + (1.0 - fi) * (1.0 - uu);
+        let iweight = u - fi;
+
         for j in 0..2 {
             let fj = j as f64;
-            let jterm = fj * v + (1.0 - fj) * (1.0 - v);
+            let jterm = fj * vv + (1.0 - fj) * (1.0 - vv);
+            let jweight = v - fj;
+
             for k in 0..2 {
                 let fk = k as f64;
-                let kterm = fk * w + (1.0 - fk) * (1.0 - w);
-                accum += iterm * jterm * kterm * c[i][j][k];
+                let kterm = fk * ww + (1.0 - fk) * (1.0 - ww);
+                let kweight = w - fk;
+
+                let weight_v = Vec3(iweight, jweight, kweight);
+                accum += iterm * jterm * kterm * c[i][j][k].dot(weight_v);
             }
         }
     }
