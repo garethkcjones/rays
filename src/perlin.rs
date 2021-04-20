d1421560a455325ddf3db100affda36e24e2da1a
Gareth Jones <gareth.k.jones@gmail.com>
2021-04-20 20:15:57 +0100

    Port RTTNW Listing 33 to Rust

--- src/perlin.rs
+++ src/perlin.rs
@@ -22,14 +22,45 @@ impl Perlin {
 
     #[must_use]
     pub fn noise(&self, p: Vec3) -> f64 {
+        #![allow(clippy::many_single_char_names)]
+
         const MASK: usize = POINT_COUNT - 1;
 
-        let i = (4.0 * p.x()) as isize as usize & MASK;
-        let j = (4.0 * p.y()) as isize as usize & MASK;
-        let k = (4.0 * p.z()) as isize as usize & MASK;
+        let Vec3(px, py, pz) = p;
 
-        let ind = self.perm_x[i] ^ self.perm_y[j] ^ self.perm_z[k];
-        self.ranfloat[ind]
+        let fpx = px.floor();
+        let fpy = py.floor();
+        let fpz = pz.floor();
+
+        let u = px - fpx;
+        let v = py - fpy;
+        let w = pz - fpz;
+
+        let i = fpx as i32;
+        let j = fpy as i32;
+        let k = fpz as i32;
+
+        let mut c = [[[0.0; 2]; 2]; 2];
+
+        for di in 0..2 {
+            let iterm = (i + di) as isize as usize & MASK;
+            let xterm = self.perm_x[iterm];
+
+            for dj in 0..2 {
+                let jterm = (j + dj) as isize as usize & MASK;
+                let yterm = self.perm_y[jterm];
+
+                for dk in 0..2 {
+                    let kterm = (k + dk) as isize as usize & MASK;
+                    let zterm = self.perm_z[kterm];
+
+                    let ind = xterm ^ yterm ^ zterm;
+                    c[di as usize][dj as usize][dk as usize] = self.ranfloat[ind];
+                }
+            }
+        }
+
+        trilinear_interp(&c, u, v, w)
     }
 }
 
@@ -65,4 +96,24 @@ fn perlin_generate_perm() -> [usize; POINT_COUNT] {
     }
     permute(&mut p);
     p
+}
+
+fn trilinear_interp(c: &[[[f64; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+    let mut accum = 0.0;
+
+    #[allow(clippy::needless_range_loop)]
+    for i in 0..2 {
+        let fi = i as f64;
+        let iterm = fi * u + (1.0 - fi) * (1.0 - u);
+        for j in 0..2 {
+            let fj = j as f64;
+            let jterm = fj * v + (1.0 - fj) * (1.0 - v);
+            for k in 0..2 {
+                let fk = k as f64;
+                let kterm = fk * w + (1.0 - fk) * (1.0 - w);
+                accum += iterm * jterm * kterm * c[i][j][k];
+            }
+        }
+    }
+    accum
 }
