77edbe34ba379161897ca0d9028ed45bf05e54c1
Gareth Jones <gareth.k.jones@gmail.com>
2021-04-20 19:55:43 +0100

    RTTNW Listing 33
    Add trilinear interpolation smoothing to Perlin noise.

--- src/perlin.cc
+++ src/perlin.cc
@@ -2,6 +2,7 @@
 
 #include <array>
 #include <cassert>
+#include <cmath>
 #include <cstddef>
 #include <numeric>
 #include <random>
@@ -46,6 +47,26 @@ namespace {
 			i = rand_dst(rand_eng);
 		return ranfloat;
 	}
+
+	auto trilinear_interp(
+		std::array<std::array<std::array<double, 2>, 2>, 2> const &c,
+		double const u,
+		double const v,
+		double const w
+	) -> double {
+		auto accum = 0.0;
+		for (auto i = 0; i < 2; ++i) {
+			auto const iterm = i * u + (1.0 - i) * (1.0 - u);
+			for (auto j = 0; j < 2; ++j) {
+				auto const jterm = j * v + (1.0 - j) * (1.0 - v);
+				for (auto k = 0; k < 2; ++k) {
+					auto const kterm = k * w + (1.0 - k) * (1.0 - w);
+					accum += iterm * jterm * kterm * c[i][j][k];
+				}
+			}
+		}
+		return accum;
+	}
 }
 
 Perlin::Perlin(std::default_random_engine &rand_eng):
@@ -59,15 +80,41 @@ Perlin::Perlin(std::default_random_engine &rand_eng):
 auto Perlin::noise(Vec3 const p) const noexcept -> double {
 	constexpr auto mask = Perlin::point_count - 1;
 
-	auto const i = static_cast<std::size_t>(4.0 * p.x) & mask;
-	auto const j = static_cast<std::size_t>(4.0 * p.y) & mask;
-	auto const k = static_cast<std::size_t>(4.0 * p.z) & mask;
+	auto const fpx = std::floor(p.x);
+	auto const fpy = std::floor(p.y);
+	auto const fpz = std::floor(p.z);
 
-	assert(i < perm_x_.size());
-	assert(j < perm_y_.size());
-	assert(k < perm_z_.size());
+	auto const u = p.x - fpx;
+	auto const v = p.y - fpy;
+	auto const w = p.z - fpz;
 
-	auto const ind = perm_x_[i] ^ perm_y_[j] ^ perm_z_[k];
-	assert(ind < ranfloat_.size());
-	return ranfloat_[ind];
+	auto const i = static_cast<int>(fpx);
+	auto const j = static_cast<int>(fpy);
+	auto const k = static_cast<int>(fpz);
+
+	std::array<std::array<std::array<double, 2>, 2>, 2> c;
+
+	for (auto di = 0; di < 2; ++di) {
+		auto const iterm = static_cast<std::size_t>(i + di) & mask;
+		assert(iterm < perm_x_.size());
+		auto const xterm = perm_x_[iterm];
+
+		for (auto dj = 0; dj < 2; ++dj) {
+			auto const jterm = static_cast<std::size_t>(j + dj) & mask;
+			assert(jterm < perm_y_.size());
+			auto const yterm = perm_y_[jterm];
+
+			for (auto dk = 0; dk < 2; ++dk) {
+				auto const kterm = static_cast<std::size_t>(k + dk) & mask;
+				assert(kterm < perm_z_.size());
+				auto const zterm = perm_z_[kterm];
+
+				auto const ind = xterm ^ yterm ^ zterm;
+				assert(ind < ranfloat_.size());
+				c[di][dj][dk] = ranfloat_[ind];
+			}
+		}
+	}
+
+	return trilinear_interp(c, u, v, w);
 }
