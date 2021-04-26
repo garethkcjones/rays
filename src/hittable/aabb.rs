bf057f32eb948a4a0eafcdf5ddf1008e4f570699
Gareth Jones <gareth.k.jones@gmail.com>
2021-04-26 12:59:55 +0100

    Port RTTNW Listing 14 to Rust

--- src/hittable/aabb.rs
+++ src/hittable/aabb.rs
@@ -27,6 +27,19 @@ impl Aabb {
     }
 
     #[must_use]
+    pub fn surrounding_box(box0: &Self, box1: &Self) -> Self {
+        let Vec3(b0mnx, b0mny, b0mnz) = box0.minimum;
+        let Vec3(b0mxx, b0mxy, b0mxz) = box0.maximum;
+        let Vec3(b1mnx, b1mny, b1mnz) = box1.minimum;
+        let Vec3(b1mxx, b1mxy, b1mxz) = box1.maximum;
+
+        let small = Vec3(b0mnx.min(b1mnx), b0mny.min(b1mny), b0mnz.min(b1mnz));
+        let big = Vec3(b0mxx.max(b1mxx), b0mxy.max(b1mxy), b0mxz.max(b1mxz));
+
+        Aabb::new(small, big)
+    }
+
+    #[must_use]
     pub fn hit(&self, r: &Ray, mut t_min: f64, mut t_max: f64) -> bool {
         let Vec3(mnx, mny, mnz) = self.minimum;
         let Vec3(mxx, mxy, mxz) = self.maximum;
